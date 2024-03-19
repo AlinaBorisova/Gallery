d981802b9cfaedb7eb01c6c4f222198afb3db350
Алина Борисова <b-alina1999.borisova@yandex.ru>
2024-03-19 14:47:40 +0300

    Correct return to main

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,12 +1,15 @@
 import style from './Logo.module.css';
 import logo from './img/logo.svg';
+import {Link} from 'react-router-dom';
 
 export const Logo = () => (
-  <a className={style.link} href="/">
-    <img
-      className={style.logo}
-      src={logo}
-      alt="Logo Gallery"
-    />
-  </a>
+  <div className={style.link}>
+    <Link to={`/`}>
+      <img
+        className={style.logo}
+        src={logo}
+        alt="Logo Gallery"
+      />
+    </Link>
+  </div>
 );
