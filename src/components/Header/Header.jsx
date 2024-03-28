437235b51269cc089551b64a6704fc0970d94d58
Алина Борисова <b-alina1999.borisova@yandex.ru>
2024-03-28 16:54:52 +0300

    Fixed styles in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import style from './Header.module.css';
 import Layout from '../Layout';
 import Logo from './Logo';
 import Auth from './Auth';
+import Search from './Search';
 
 export const Header = () => (
   <header className={style.header}>
     <Layout>
       <div className={style.header__container}>
         <Logo />
+        <Search />
         <Auth />
       </div>
     </Layout>
