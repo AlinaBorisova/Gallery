fee42205639cd8704044ef2460665cc5bf151bf1
Алина Борисова <b-alina1999.borisova@yandex.ru>
2024-04-04 11:04:35 +0300

    Added search photos

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,10 +2,11 @@ import style from './List.module.css';
 import Photos from './Photos';
 import {useDispatch, useSelector} from 'react-redux';
 import {useEffect, useRef} from 'react';
-import {Outlet, useLocation} from 'react-router-dom';
+import {Outlet, useLocation, useSearchParams} from 'react-router-dom';
 import {
   changePage,
   photosRequestAsync,
+  searchRequestAsync,
 } from '../../../store/photos/photosSlice';
 import Preloader from '../../../UI/Preloader';
 import Masonry from 'react-masonry-css';
@@ -15,6 +16,9 @@ import Masonry from 'react-masonry-css';
 export const List = () => {
   const photos = useSelector(state => state.photos.photos);
   const loadingPhotos = useSelector(state => state.photos.loadingPhotos);
+  const loadingSearch = useSelector(state => state.photos.loadingSearch);
+  const [searchParam] = useSearchParams();
+  const search = searchParam.get('search');
   const dispatch = useDispatch();
   const endList = useRef(null);
   const params = useLocation();
@@ -45,6 +49,27 @@ export const List = () => {
     }
   }, [dispatch, endList.current, params.pathname, loadingPhotos]);
 
+  useEffect(() => {
+    if (params.pathname === '/search' && !loadingSearch) {
+      const observer = new IntersectionObserver((entries, observer) => {
+        if (entries[0].isIntersecting) {
+          dispatch(searchRequestAsync(search));
+        }
+      }, {
+        rootMargin: '250px',
+      });
+      if (endList.current && !loadingSearch) {
+        observer.observe(endList.current);
+      }
+
+      return () => {
+        if (endList.current) {
+          observer.unobserve(endList.current);
+        }
+      };
+    }
+  }, [dispatch, endList.current, params.pathname, loadingSearch]);
+
   const breakpointColumnsObj = {
     default: 3,
     1100: 2,
@@ -65,7 +90,7 @@ export const List = () => {
                  <Photos photo={photo} />
                </li>
              ))}
-             {loadingPhotos && <Preloader />}
+             {(loadingPhotos || loadingSearch) && <Preloader />}
              <li ref={endList} className={style.end} />
            </Masonry>
         }
--- src/store/photos/photosSlice.js
+++ src/store/photos/photosSlice.js
@@ -3,29 +3,72 @@ import {ACCESS_KEY, URL_API} from '../../api/const';
 
 export const photosRequestAsync = createAsyncThunk(
   'fetch/fetchPhotos',
-  async (_, {getState}) => {
-    const token = getState().token.token;
-    let page = getState().photos.page;
+  async (_, {getState, rejectWithValue}) => {
+    try {
+      const token = getState().token.token;
+      let page = getState().photos.page;
+      const response = await fetch(
+        `${URL_API}/photos?per_page=30&${page && `page=${page}`}`,
+        token ?
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          } : {
+            headers: {
+              Authorization: `Client-ID ${ACCESS_KEY}`,
+            },
+          },
+      );
 
-    const response = await fetch(`
-      ${URL_API}/photos?per_page=30&${page && `page=${page}`}`,
-    token ? {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    } : {
-      headers: {
-        Authorization: `Client-ID ${ACCESS_KEY}`,
-      },
-    });
+      if (!response.ok) {
+        return rejectWithValue({
+          status: response.status,
+          error: 'Не удалось загрузить фотографии.',
+        });
+      }
+      page++;
 
-    if (!response.ok) {
-      throw new Error('Authorization error');
+      const photos = await response.json();
+      return {photos, page};
+    } catch (error) {
+      return error;
     }
-    page++;
+  });
 
-    const photos = await response.json();
-    if (photos) return {photos, page};
+export const searchRequestAsync = createAsyncThunk(
+  'fetch/fetchSearch',
+  async (search, {getState, rejectWithValue}) => {
+    try {
+      const token = getState().token.token;
+      let page = getState().photos.page;
+      const response = await fetch(
+        `${URL_API}/search/photos?per_page=30&page=${page}&query=${search}`,
+        token ?
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          } : {
+            headers: {
+              Authorization: `Client-ID ${ACCESS_KEY}`,
+            },
+          },
+      );
+
+      if (!response.ok) {
+        return rejectWithValue({
+          status: response.status,
+          error: 'Не удалось загрузить фотографии.',
+        });
+      }
+      page++;
+
+      const photos = await response.json();
+      return {photos, page};
+    } catch (error) {
+      return error;
+    }
   });
 
 const initialState = {
@@ -35,6 +78,9 @@ const initialState = {
   page: 1,
   params: '',
   loadingPhotos: false,
+  loadingSearch: false,
+  search: '',
+  pageSearch: 1,
 };
 
 const photosSlice = createSlice({
@@ -43,32 +89,58 @@ const photosSlice = createSlice({
   reducers: {
     changePage: (state, action) => {
       state.photos = [];
-      state.error = '';
+      state.loading = false;
+      state.error = null;
       state.page = 1;
+      state.pageSearch = 1;
       state.params = action.payload;
+      state.search = '';
     },
   },
   extraReducers: (builder) => {
     builder
-      .addCase(photosRequestAsync.pending, state => {
-        state.loadingPhotos = true;
-        state.status = 'loading';
-        state.error = '';
+      .addCase(photosRequestAsync.pending, (state) => {
+        state.loadingPhoto = true;
+        state.error = null;
+        state.status = '';
       })
       .addCase(photosRequestAsync.fulfilled, (state, action) => {
         state.photos = [...state.photos, ...action.payload.photos];
-        state.loadingPhotos = false;
-        state.status = 'loaded';
+        state.loadingPhoto = false;
+        state.error = null;
         state.page = action.payload.page;
-        state.error = '';
+        state.status = '';
       })
       .addCase(photosRequestAsync.rejected, (state, action) => {
-        state.loadingPhotos = false;
-        state.status = 'error';
-        state.error = action.error;
+        state.loadingPhoto = false;
+        state.error = action.payload.error;
+        state.status = action.payload.status;
+      })
+      .addCase(searchRequestAsync.pending, (state) => {
+        state.loadingSearch = true;
+        state.error = null;
+        state.status = '';
+      })
+      .addCase(searchRequestAsync.fulfilled, (state, action) => {
+        if (state.search === action.meta.arg) {
+          state.photos = [...state.photos, ...action.payload.photos.results];
+        } else {
+          state.photos = action.payload.photos.results;
+          state.search = action.meta.arg;
+        }
+        state.loadingSearch = false;
+        state.error = null;
+        state.page = action.payload.page;
+        state.status = '';
+      })
+      .addCase(searchRequestAsync.rejected, (state, action) => {
+        state.loadingSearch = false;
+        state.error = action.payload.error;
+        state.status = action.payload.status;
       });
-  }
+  },
 });
+
 
 export const {changePage} = photosSlice.actions;
 export default photosSlice.reducer;
