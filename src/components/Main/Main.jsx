cbd55b15a3b3eebcab9bc4dc696c0161275ba02a
Алина Борисова <b-alina1999.borisova@yandex.ru>
2024-04-10 20:28:24 +0300

    Added pages route

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,14 +3,17 @@ import List from './List';
 import Layout from '../Layout';
 import {Route, Routes} from 'react-router-dom';
 import {PhotoPage} from '../PhotoPage/PhotoPage';
+import NotFound from '../NotFound';
 
 export const Main = () => (
   <main className={style.main}>
     <Layout>
       <Routes>
         <Route path='/auth' element={<List />} />
-        <Route path='*' element={<List />} />
-        <Route path='photo/:id' element={<PhotoPage />} />
+        <Route path='/' element={<List />} />
+        <Route path='/photo/:id' element={<PhotoPage />} />
+        <Route path='/favorite' element={<List />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Layout>
   </main>
