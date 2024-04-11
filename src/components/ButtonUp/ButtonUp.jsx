57960e297e869c53d99c0eda678d2e72d5971f1a
Алина Борисова <b-alina1999.borisova@yandex.ru>
2024-04-11 11:30:21 +0300

    Fixed buttonUp

--- src/components/ButtonUp/ButtonUp.jsx
+++ src/components/ButtonUp/ButtonUp.jsx
@@ -1,14 +1,10 @@
 import style from './ButtonUp.module.css';
 import {ReactComponent as ArrowIcon} from './img/arrowUp.svg';
-import {useLocation, useNavigate} from 'react-router-dom';
 
 export const ButtonUp = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
   const handleClick = () => {
-    navigate(location.pathname);
+    window.scrollTo(0, 0);
   };
-
 
   return (
     <div className={style.container} >
